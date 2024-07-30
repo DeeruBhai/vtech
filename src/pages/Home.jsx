37d6a0e2ed759d9acd919501eb1b3d98cca48b16
DeeruBhai <deerajalagarsamy2001@gmail.com>
2024-07-30 23:42:41 +0530

    home and navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import banner from "../assets/banner.png";
+import closeicon from "../assets/close-circle-svgrepo-com.svg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      className={` w-full min-[525px]:h-screen  h-2/5 bg-hero bg-no-repeat bg-cover flex justify-center items-center `}
+    >
+      <div className="w-full h-full flex flex-col bg-white/50 justify-center items-center">
+        <div className="uppercase text-6xl text-blue-800 font-extrabold">
+          {" "}
+          vtech services pvt ltd
+        </div>
+        <div className="uppercase text-lg mt-8 text-white font-bold bg-blue-800 w-1/3 text-center">
+          You imagine we create
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
